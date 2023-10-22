b500f2ae6210598d5dab9a491d6a8b146c668a81
VowcicefskiJ <96920791+VowcicefskiJ@users.noreply.github.com>
2023-10-22 19:33:27 -0400

    Add files via upload

---
+++ Arrays/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main() {
+    
+char vowels[] {'a','e','i','o','u'};
+    cout << "\nThe first vowel is: "<<vowels[0]<<endl;
+    cout << "The last vowel is:" <<vowels[4]<<endl;
+    
+ //   cin >> vowels[5];
+ 
+    double hi_temps[] {90.1,89.8,77.5,81.6};
+    cout<<"\nThe first high temperature is:"<<hi_temps[0]<<endl;
+    
+    hi_temps[0]=100.7;
+    
+    cout<<"The first high temperature is now:"<<hi_temps[0]<<endl;
+    
+    
+    
+    int test_scores[]{100,90,80,70,60};
+    
+    cout<<"\nFrist score at index 0:"<<test_scores[0]<<endl;
+    cout<<"Second score at index 1:"<<test_scores[1]<<endl;
+    cout<<"Third score at index 2:"<<test_scores[2]<<endl;
+    cout<<"Fourth score at index 3:"<<test_scores[3]<<endl;
+    cout<<"Fifth score at index 4:"<<test_scores[4]<<endl;
+}
